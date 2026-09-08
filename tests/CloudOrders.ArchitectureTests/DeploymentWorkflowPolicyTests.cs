c9c1ea73e70e44d2d7c6c401dd7c96d25a815263
RobertMagowan <robert.magowan@rabmsolutions.co.uk>
2026-09-08 06:04:55 +0100

    fix: quiesce latest ingress traffic

--- tests/CloudOrders.ArchitectureTests/DeploymentWorkflowPolicyTests.cs
+++ tests/CloudOrders.ArchitectureTests/DeploymentWorkflowPolicyTests.cs
@@ -451,8 +451,8 @@ public sealed class DeploymentWorkflowPolicyTests
 
         Assert.Contains("activeRevisionsMode: 'Multiple'", containerAppModule, StringComparison.Ordinal);
         Assert.Contains("az containerapp revision set-mode --name \"$AZURE_APP_NAME\" --resource-group \"$AZURE_RESOURCE_GROUP\" --mode multiple", quiesceStep.Value, StringComparison.Ordinal);
-        Assert.Contains("--arg before \"$BEFORE_REVISION\"", quiesceStep.Value, StringComparison.Ordinal);
-        Assert.Contains("elif .latestRevision then $before", quiesceStep.Value, StringComparison.Ordinal);
+        Assert.Contains("if .latestRevision then \"latest=0\"", quiesceStep.Value, StringComparison.Ordinal);
+        Assert.DoesNotContain("elif .latestRevision then $before", quiesceStep.Value, StringComparison.Ordinal);
         Assert.Contains("az containerapp ingress traffic set", quiesceStep.Value, StringComparison.Ordinal);
         Assert.Contains("jq -e 'all(.[]; (.weight // 0) == 0)'", quiesceStep.Value, StringComparison.Ordinal);
     }
